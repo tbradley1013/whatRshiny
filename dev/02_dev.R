4f5e4497a0f6ea5df03a8975b88ee223d1b711d8
Tyler Bradley <tyler.bradley@phila.gov>
2020-02-19 08:04:35 -0500

    adding pipe

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -15,6 +15,8 @@ golem::add_module( name = "my_other_module" ) # Name of the module
 usethis::use_package( "shiny" ) # To call each time you need a new package
 usethis::use_dev_package("whatr")
 usethis::use_package("dplyr")
+usethis::use_pipe()
+
 ## 2.3 Add tests
 
 usethis::use_test( "app" )
