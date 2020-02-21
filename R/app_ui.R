000872fee97bf22c5fdc37b55a38cd57e0c19df0
Tyler Bradley <tyler.bradley@phila.gov>
2020-02-20 21:29:55 -0500

    removing default stuff

--- R/app_ui.R
+++ R/app_ui.R
@@ -6,7 +6,6 @@ app_ui <- function() {
     shinyjs::useShinyjs(),
     waiter::use_waiter(),
     waiter::waiter_show_on_load(custom_loading_spinner(), color = "#0C00A8"),
-    # List the first level UI elements here 
     fluidPage(
       h1("Shiny Jeopardy!"),
       h3("Replay past Jeopardy games using the whatR and whatRshiny packages!"),
@@ -35,7 +34,6 @@ app_ui <- function() {
           purrr::map(1:5, function(z){
             div(
               class = "board-row",
-              # display = "table-row",
               purrr::map(1:6, ~{
                 mod_ind_box_ui(id = paste("ind_box_ui_1", z, .x, sep = "_"))
               })
@@ -49,7 +47,6 @@ app_ui <- function() {
             purrr::map(1:5, function(z){
               div(
                 class = "board-row",
-                # display = "table-row",
                 purrr::map(1:6, ~{
                   mod_ind_box_ui(id = paste("ind_box_ui_2", z, .x, sep = "_"))
                 })
@@ -62,10 +59,6 @@ app_ui <- function() {
             id = "final-jeapordy"
           )
         )
-        # display = "table",
-        
-        
-        # mod_ind_box_ui("ind_box_ui_1")
       ),
       uiOutput("score")
     )
@@ -82,9 +75,6 @@ golem_add_external_resources <- function(){
   tags$head(
     golem::activate_js(),
     golem::favicon(),
-    # Add here all the external resources
-    # If you have a custom.css in the inst/app/www
-    # Or for example, you can add shinyalert::useShinyalert() here
     tags$link(rel="stylesheet", type="text/css", href="www/styles.css"),
     tags$script(src = "www/button-click.js")
   )
